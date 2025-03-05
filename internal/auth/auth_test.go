ef85b81c21f71f0b4afc90cdb043ae8b93258543
coleYab <yabume123@gmail.com>
2025-03-05 08:27:21 +0000

    add: adding test for authorization file'

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"testing"
+)
+
+func TestGetToken_InvalidCredentialsSandbox(t *testing.T) {
+	token := New("invalidConsumerKey", "invalidConsumerSecret")
+
+	env := "SANDBOX"
+
+	_, err := token.GetToken(env)
+	if err == nil {
+		t.Fatalf("Expected an error, but got none")
+	}
+}
+
+func TestGetToken_InvalidCredentials(t *testing.T) {
+	token := New("invalidConsumerKey", "invalidConsumerSecret")
+	env := "PRODUCTION"
+	_, err := token.GetToken(env)
+	if err == nil {
+		t.Fatalf("Expected an error, but got none")
+	}
+}
+
+
